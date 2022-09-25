9416599b3a975932bc84a9158cbb96eac8c3a5cf
Jonathan Perez <jonathanperezvoulquin@gmail.com>
2022-09-25 20:43:38 -0300

    Migrations tables and add tables admin site

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from clients.models import ListaDeClientes, agregar
 # Register your models here.
+class Client(admin.ModelAdmin):
+    list_display = ('id', 'nombre_apellido', 'direccion', 'telefono','ultimo_lavado','precio','detalle','email')
+    class Meta:
+        managed = False
+        db_table = 'lista_de_clientes_'
+
+class Add(admin.ModelAdmin):
+    list_display = ('first_name','last_name')
+
+admin.site.register(ListaDeClientes,Client)
+admin.site.register(agregar, Add)
+
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Register your models here.
+
+class ListaDeClientes(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_apellido = models.CharField(db_column='Nombre_Apellido', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    direccion = models.CharField(db_column='Direccion', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    telefono = models.CharField(db_column='Telefono', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    ultimo_lavado = models.DateField(db_column='Ultimo_lavado', blank=True, null=True)  # Field name made lowercase.
+    precio = models.DecimalField(max_digits=50, decimal_places=0, blank=True, null=True)
+    detalle = models.CharField(db_column='Detalle', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    email = models.CharField(db_column='Email', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'lista_de_clientes_'
+
+
+
+class agregar(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
